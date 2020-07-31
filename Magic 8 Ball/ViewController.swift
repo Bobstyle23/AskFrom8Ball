00e9df2b775ed8fb3697bbc2e39db9d5289fd890
Mukhammad Bobur Pakhriev <pahriyev4888@mail.ru>
2020-07-31 23:24:44 +0900

    Initial commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,7 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    var advice: String = ""
+    
+    let languageArray = [#imageLiteral(resourceName: "Swift"), #imageLiteral(resourceName: "JavaScript"), #imageLiteral(resourceName: "Ruby"), #imageLiteral(resourceName: "python"), #imageLiteral(resourceName: "C_C++"), #imageLiteral(resourceName: "Java")]
+    
+    override func viewDidLoad() {
+        imageView.image = languageArray[Int.random(in: 0...5)]
+    }
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = languageArray[Int.random(in: 0...5)]
+        learnAdvice()
+    }
+    
+    func learnAdvice() {
+        if imageView.image == languageArray[0] {
+            adviceLabel.text = "Swift"
+        }
+        else if imageView.image == languageArray[1] {
+            adviceLabel.text = "JavaScript"
+        }
+        else if imageView.image == languageArray[2] {
+             adviceLabel.text = "Ruby"
+        }
+        else if imageView.image == languageArray[3] {
+             adviceLabel.text = "Python"
+        }
+        else if imageView.image == languageArray[4] {
+             adviceLabel.text = "C/C++"
+        }
+        else {
+             adviceLabel.text = "Java"
+        }
+        
+    }
 
 
 
